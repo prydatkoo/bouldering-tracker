8704957800ecea049d4f38ded696e997f690d643
Maryan <maryanko777@gmail.com>
2026-03-30 22:48:25 +0200

    fix: typo

--- server/routes/climbs.pages.routes.js
+++ server/routes/climbs.pages.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { getAllClimbs } = require("../models/climbs.models");
 
-router.get("climbs", async (req, res, next) => {
+router.get("/climbs", async (req, res, next) => {
   try {
     const climbs = await getAllClimbs();
-    res.render("/climbs/index", { climbs });
+    res.render("climbs/index", { climbs });
   } catch (err) {
     next(err);
   }
