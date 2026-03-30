c4f880878ac2756b6499d4dc41a5d554858467a3
Maryan <maryanko777@gmail.com>
2026-03-30 23:02:34 +0200

    feat: update model, add a second function

--- server/models/climbs.models.js
+++ server/models/climbs.models.js
@@ -7,4 +7,12 @@ async function getAllClimbs() {
   return result.rows;
 }
 
+async function createClimb(name, grade) {
+  const result = await query(
+    "INSERT INTO climbs (name, grade) VALUES ($1, $2) RETURNING id, name, grade, created_at",
+    [name, grade],
+  );
+  return result.rows[0];
+}
+
 module.exports = { getAllClimbs };
