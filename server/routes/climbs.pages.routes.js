06a1a2ccd5cb4d62fc3c74eee3d7449160218eee
Maryan <maryanko777@gmail.com>
2026-03-30 23:34:42 +0200

    feat: add edit and update climbs page routes

--- server/routes/climbs.pages.routes.js
+++ server/routes/climbs.pages.routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { getAllClimbs, createClimb } = require("../models/climbs.models");
+const {
+  getAllClimbs,
+  createClimb,
+  updateClimb,
+} = require("../models/climbs.models");
 
 router.get("/climbs", async (req, res, next) => {
   try {
@@ -15,6 +19,17 @@ router.get("/climbs/new", async (req, res, next) => {
   res.render("climbs/new");
 });
 
+router.get("/clibs/:id/edit", async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    const climb = await getClimbById(id);
+    if (!climb) return res.status(404).send("Climb not found");
+    res.render("climbs/edit", { climb });
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.post("/climbs", async (req, res, next) => {
   try {
     const { name, grade } = req.body;
@@ -22,6 +37,22 @@ router.post("/climbs", async (req, res, next) => {
       return res.status(400).send("Name and grade are required");
     await createClimb(name, grade);
     res.redirect("/climbs");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/climbs/:id", async (req, res, next) => {
+  try {
+    const id = Number(req.params.id);
+    const { name, grade } = req.body;
+    if (!name || !grade) {
+      return res.status(400).send("Name and grade are required");
+    }
+    const updated = await updateClimb(id, name, grade);
+    if (!updated) return updateClimb(id, name, grade);
+    if (!updated) return res.status(404).send("Climb not found");
+    res.redirect("climbs");
   } catch (err) {
     next(err);
   }
