f48084acb6c3937a81ee6e39750d5545ad2d273c
Maryan <maryanko777@gmail.com>
2026-03-30 23:06:10 +0200

    feat: update page routes

--- server/routes/climbs.pages.routes.js
+++ server/routes/climbs.pages.routes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { getAllClimbs } = require("../models/climbs.models");
+const { getAllClimbs, createClimb } = require("../models/climbs.models");
 
 router.get("/climbs", async (req, res, next) => {
   try {
     const climbs = await getAllClimbs();
     res.render("climbs/index", { climbs });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/climbs/new", async (req, res, next) => {
+  res.render("climbs/new");
+});
+
+router.post("/climbs", async (req, res, next) => {
+  try {
+    const { name, grade } = req.body;
+    if (!name || !grade)
+      return res.status(400).send("Name and grade are required");
+    await createClimb(name, grade);
+    res.redirect("/climbs");
   } catch (err) {
     next(err);
   }
