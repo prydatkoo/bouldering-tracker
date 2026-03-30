f67b77ef4a2025d3771e9a8f19261e8f52426e89
Maryan <maryanko777@gmail.com>
2026-03-30 22:48:53 +0200

    feat: update app.js to include new routes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const pagesRouter = require("./routes/pages.routes");
 const climbsRouter = require("./routes/climbs.routes");
+const climbsPagesRouter = require("./routes/climbs.pages.routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../client")));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/", pagesRouter);
+app.use("/", climbsPagesRouter);
 app.use("/api/climbs", climbsRouter);
 
 app.use((req, res) => {
